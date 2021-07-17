5dfa3aab77f069a2438a373ed98600d419abb8cc
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-17 18:53:07 -0500

    Prettier configured

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,17 +7,17 @@ import ClotheStoreIcon from '../../assets/images/ClothesStore.png'
 import './Header.scss'
 
 const Header = () => {
-    return (
-        <header className="header">
-                <img src={ClotheStoreIcon} alt="clothesstore icon" className="icon"/>
-                <SearchBar/>
-                <div className="buttons-container">
-                <ShoppingCartButton className="shooping-cart"/>
-                <UserIcon className="userIcon"/>
-                <Button type="-secondary" label="Iniciar sesión" className="login-button"/>
-                </div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img src={ClotheStoreIcon} alt="clothesstore icon" className="icon" />
+      <SearchBar />
+      <div className="buttons-container">
+        <ShoppingCartButton className="shooping-cart" />
+        <UserIcon className="userIcon" />
+        <Button type="-secondary" label="Iniciar sesión" className="login-button" />
+      </div>
+    </header>
+  )
 }
 
 export default Header
