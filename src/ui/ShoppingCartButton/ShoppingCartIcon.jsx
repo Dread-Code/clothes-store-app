871e5300bb68c9a4d5f68b9b99c9bf435d6fa2cd
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-17 17:29:27 -0500

    Shopping cart ui

---
+++ src/ui/ShoppingCartButton/ShoppingCartIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ShoppingCartIcon from '../../../assets/icons/icon-cart.svg'
+import './ShoppingCartButton.scss'
+
+const ShoppingCartButton = ({ numberOfItems, action }) => (
+  <div className="a-header__shoppingCartButton">
+    <button type="button" onClick={action}>
+      <ShoppingCartIcon />
+    </button>
+    {numberOfItems > 0 && (
+      <div className="burble">
+        <span>{numberOfItems}</span>
+      </div>
+    )}
+  </div>
+)
+
+ShoppingCartButton.propTypes = {
+  numberOfItems: PropTypes.number.isRequired,
+  action: PropTypes.func.isRequired
+}
+
+export default ShoppingCartButton
