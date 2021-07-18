11e4689a0db75c3f736c3e4e97829cf6f3c20d85
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-18 18:56:50 -0500

    Transform-runtime added and redux implemented

---
+++ src/redux/actions/ui.js
@@ -0,0 +1,5 @@
+import { types } from '../types'
+
+export const showModal = () => ({ types: types.UI_SHOW_MODAL })
+
+export const hideModal = () => ({ types: types.UI_HIDE_MODAL })
---
+++ src/redux/types/index.js
@@ -0,0 +1,5 @@
+export const types = {
+  /** [ui] Action Types */
+  UI_SHOW_MODAL: '[UI] SHOW_MODAL',
+  UI_HIDE_MODAL: '[UI] HIDE_MODAL'
+}
