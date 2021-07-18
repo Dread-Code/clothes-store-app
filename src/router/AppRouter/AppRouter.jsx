2b23110875f03e001f587060894fef605f4d766e
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-17 20:53:23 -0500

    NavBar Added

--- src/router/AppRouter/AppRouter.jsx
+++ src/router/AppRouter/AppRouter.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from '../../components/Header'
+import Home from '../../components/Home'
+import Navbar from '../../components/Navbar'
 import Accesories from '../../layouts/Accesories'
 import Child from '../../layouts/Child'
+import Junior from '../../layouts/Junior'
 import Men from '../../layouts/Men'
-import Women from '../../layouts/Women/Women'
+import Offers from '../../layouts/Offers'
+import Women from '../../layouts/Women'
 import './AppRouter.scss'
 
 const RouterApp = () => (
   <>
     <Header />
+    <Navbar />
     <div className="container">
       <div>
         <Switch>
@@ -17,7 +22,9 @@ const RouterApp = () => (
           <Route path="/child" component={Child} />
           <Route path="/men" component={Men} />
           <Route path="/men" component={Women} />
-          <Route path="/" component={Women} />
+          <Route path="/junior" component={Junior} />
+          <Route path="/offers" component={Offers} />
+          <Route path="/" component={Home} />
         </Switch>
       </div>
     </div>
