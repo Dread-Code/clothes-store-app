5f4432aff19f8721f68929ed70015b3bc12fdf4a
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-19 12:19:40 -0500

    Icon route to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useHistory } from 'react-router'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import Button from '../Button'
 import SearchBar from '../SearchBar'
@@ -7,24 +8,33 @@ import UserIcon from '../../assets/icons/icon-user.svg'
 import ShoppingCartButton from '../ShoppingCartButton/ShoppingCartIcon'
 import './Header.scss'
 
-const Header = ({ handlerClick }) => (
-  <header className="header">
-    <span role="button" tabIndex={0} className="hamburger-menu" onClick={handlerClick}>
-      <GiHamburgerMenu />
-    </span>
-    <img
-      src="https://user-images.githubusercontent.com/60331479/126054130-dc26d1f3-cab6-40fb-a84a-97fe65450ee1.png"
-      alt="clothesstore icon"
-      className="icon"
-    />
-    <SearchBar className="searchbar" />
-    <div className="buttons-container">
-      <ShoppingCartButton className="shooping-cart" />
-      <UserIcon className="userIcon" />
-      <Button type="-secondary" label="Iniciar sesión" className="login-button" />
-    </div>
-  </header>
-)
+const Header = ({ handlerClick }) => {
+  const history = useHistory()
+  const handleClick = () => {
+    history.push('/')
+  }
+
+  return (
+    <header className="header">
+      <span role="button" tabIndex={0} className="hamburger-menu" onClick={handlerClick}>
+        <GiHamburgerMenu />
+      </span>
+      <img
+        src="https://user-images.githubusercontent.com/60331479/126054130-dc26d1f3-cab6-40fb-a84a-97fe65450ee1.png"
+        alt="clothesstore icon"
+        className="icon"
+        role="button"
+        onClick={handleClick}
+      />
+      <SearchBar className="searchbar" />
+      <div className="buttons-container">
+        <ShoppingCartButton className="shooping-cart" />
+        <UserIcon className="userIcon" />
+        <Button type="-secondary" label="Iniciar sesión" className="login-button" />
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   handlerClick: PropTypes.func.isRequired
