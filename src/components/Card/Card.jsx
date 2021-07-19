44f1b557afb3e9b35c67855918515c1faf834229
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-18 22:08:48 -0500

    Card styles fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Button from '../Button'
 import './Card.scss'
 
-const Card = () => (
+const Card = ({ id, price, nameProduct, imageUrl }) => (
   <div className="card">
     <div className="discount" />
     <div
       style={{
-        backgroundImage: `url("https://user-images.githubusercontent.com/60331479/126054321-e020a530-0af0-4c3a-8e1a-cb44688f04e7.png")`
+        backgroundImage: `url("${imageUrl}")`
       }}
       className="card-image"
       alt="asdfasf"
     />
     <div className="card-content">
-      <h3 className="card-title">Pantalón silueta skinny color sólido</h3>
+      <h3 className="card-title">{nameProduct}</h3>
       <div>
-        <span className="price-primary">$200.452</span>
+        <span className="price-primary">{`${price}`}</span>
         <span className="price-secondary">$100.420</span>
       </div>
       <Button label="Agregar al carrito" cart={true} />
