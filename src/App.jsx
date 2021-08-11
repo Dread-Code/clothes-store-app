b9c0aab743544289c6467e5b0517205459d24dfe
DreadfulCrazy <lucasospina97@gmail.com>
2021-08-11 10:34:07 -0500

    browser route test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
 import { Provider } from 'react-redux'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import MainPage from './pages/MainPage/MainPage'
 import store from './redux/store'
 
